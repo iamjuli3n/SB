e9cb2ad0a88e4125825d7097e88c8f271e4c556c
Julien <93400859+iamjuli3n@users.noreply.github.com>
2024-12-08 15:42:16 -0500

    Lightbox!!!!
    "Implemented a lightbox feature on the site. Currently working on improving slide-to-slide transitions to make them smoother and more fluid. Further troubleshooting is needed to refine the user experience."

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,6 +81,30 @@ jQuery("header ul li a").each(function () {
 jQuery(window).on("scroll", onScroll);
 
 
+
+
+
+var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+var cardWidth = $(".carousel-item").width();
+var scrollPosition = 0;
+$(".carousel-control-next").on("click", function () {
+    if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+      scrollPosition += cardWidth;  //update scroll position
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+    }
+});
+
+$(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $(".carousel-inner").animate(
+            { scrollLeft: scrollPosition },
+            600
+        );
+    }
+});
+
+
 // =============== DISABLE INSPECT & SAVE CODE ===============
 // document.addEventListener('contextmenu', function (event) {
 //     event.preventDefault();
